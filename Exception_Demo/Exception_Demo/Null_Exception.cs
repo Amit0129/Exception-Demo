a1c0a5b4d049b63e30d8f89e61afaffb85556eb5
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-18 23:05:08 +0530

    [Amit]Add : Executing inbuild NullReference Exception

---
+++ Exception_Demo/Exception_Demo/Null_Exception.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Demo
+{
+    public class Null_Exception
+    {
+        public void NullRefException()
+        {
+            //try
+            //{
+            //    string str = null;
+            //    Console.WriteLine(str.Substring(5));
+            //}
+            //catch (NullReferenceException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            string str = null;
+            if (str != null)
+            {
+                Console.WriteLine(str.Substring(5));
+            }
+            else
+            {
+                Console.WriteLine("String is null");
+            }
+        }
+       
+    }
+}
--- Exception_Demo/Exception_Demo/Program.cs
+++ Exception_Demo/Exception_Demo/Program.cs
@@ -11,9 +11,11 @@
             {
                 Console.WriteLine("1.Eception handle");
                 Console.WriteLine("2.Divied By Zero Inbuild Eception");
-                Console.WriteLine("0.Exit");
+                Console.WriteLine("3.null Reference Inbuild Eception");
+                Console.WriteLine("0.Exit\n");
                 Console.WriteLine("Please choose an above Option\n");
                 int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
                 switch (option)
                 {
                     case 0:
@@ -26,6 +28,10 @@
                     case 2:
                         Devied_By_Zero_Exception devied_By_Zero = new Devied_By_Zero_Exception();
                         devied_By_Zero.Costom();
+                        break;
+                    case 3:
+                        Null_Exception null_Exception = new Null_Exception();
+                        null_Exception.NullRefException();
                         break;
                     default:
                         Console.WriteLine("Please choose an Above Option");
