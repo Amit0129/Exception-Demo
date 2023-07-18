cceb3e28675d2182118dad967bb09594200b44a6
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-18 21:11:45 +0530

    [Amit]Add : Executing inbuild DiviedByZero Exception

---
+++ Exception_Demo/Exception_Demo/Devied_By_Zero_Exception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Demo
+{
+    public class Devied_By_Zero_Exception
+    {
+        public void Costom()
+        {
+            try
+            {
+                int a = 0;
+                int b = 100 / a;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- Exception_Demo/Exception_Demo/Program.cs
+++ Exception_Demo/Exception_Demo/Program.cs
@@ -4,9 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            ExceptionHandle exception = new ExceptionHandle();
-            exception.Display();
+           
+
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("1.Eception handle");
+                Console.WriteLine("2.Divied By Zero Inbuild Eception");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("Please choose an above Option\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        check = false;
+                        break;
+                    case 1:
+                        ExceptionHandle exception = new ExceptionHandle();
+                        exception.Display();
+                        break;
+                    case 2:
+                        Devied_By_Zero_Exception devied_By_Zero = new Devied_By_Zero_Exception();
+                        devied_By_Zero.Costom();
+                        break;
+                    default:
+                        Console.WriteLine("Please choose an Above Option");
+                        break;
+                }
+            }
+            
+            
         }
     }
 }
